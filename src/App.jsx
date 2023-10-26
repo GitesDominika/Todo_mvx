4c79b062e7ca4abeb23355b31ce8c12cbf9afbde
dominika łuczyńska <dominika.luczynska83@gmail.com>
2023-10-26 13:17:02 +0200

    todo app preparing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-
+import'./App.scss';
 import {useState} from "react";
-import "./App.scss";
 
 function App() {
-const [value, setValue] = useState('')
-    const [todos, setTodos] = useState([])
 
-  function handleValue(event){
-  setValue(event.target.value);
-  }
-function handleAddTodo (){
-    if (event.key === "Enter") {
-setTodos([...value]);
-setValue('');
-    }
-}
-function  handleChangeStatus(todo) {
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
 
-}
+    const getId = () => todos.length === 0 ? 1 : Math.max(...todos.map ((task) => task.id)) + 1;
+
+    // const handleInput = (event) => {
+    //     setTodos(event.target.value)
+    //     setTodo('')
+    // };
+    const handleAppTodo = (event) => {
+        if (event.key === "Enter" && todo.trim().length >= 3) {
+            setTodos([{
+                id: getId(),
+                status: 'in progress',
+                title: todo
+            }, ...todos]);
+            setTodo('');
+
+        }
+
+    };
+    const handleChangeStatus = (task) => {
+        task.status = task.status === 'in progress' ? 'done' : 'in progress';
+        setTodos([...todos]);
+    };
+
+
     return (
-      <div>
-      <h1>todos</h1>
-                <input type="text" value={value} onChange={handleValue} onKeyUp={handleAddTodo}/>
-        <ul className="todo"> {todos.map((todo, id) => (
-            <li key={id}>
-                <span className="status" onClick={()}> </span><span>{todo}</span></li>))}</ul>
-      </div>
-  );
+        <div className="todoapp"
+        ><h1>todos</h1>
+        <sectionS className="todos">
+            <input type="text" className="todos" value={todo}
+                   placeholder="what needs to be done"
+            onChange={(event) => setTodo((event.target.value))}
+            onKeyUp={handleAppTodo}/>
+            <ul className="todos-list">
+                {todos.map((task) => (
+                    <li className="todos-iten"
+                        key={task.id}
+                    >
+                        <span className={task.status === 'in progress' ? 'status' : 'done'} onClick={ () => handleChangeStatus(task) }></span>
+                        <span>{task.title}</span></li>
+                ))};
+            </ul>
+        </sectionS>
+        </div>
+    );
 }
-export default App
+
+export default App;
